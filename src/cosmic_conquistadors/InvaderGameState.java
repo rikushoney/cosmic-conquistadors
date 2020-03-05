86831589f40d3e64eddabb4529bc6e981568c081
Rikus <contact@rikushoney.com>
2020-03-05 21:26:58 +0200

    Testing basic drawing/physics - looks good :-)

--- src/cosmic_conquistadors/InvaderGameState.java
+++ src/cosmic_conquistadors/InvaderGameState.java
@@ -1,21 +1,45 @@
 package cosmic_conquistadors;
 
 import edu.princeton.cs.introcs.StdDraw;
+import java.util.ArrayList;
 
 public class InvaderGameState {
     private Config config;
     private boolean shouldQuit;
+    private ArrayList<Critter> critters;
+
+    public InvaderGameState(Config config) {
+        this.config = config;
+        this.critters = new ArrayList<Critter>();
+        this.critters.add(new Missile(new Vector(-0.9, -1), Math.PI / 6.0));
+    }
 
     private void startRenderLoop() {
         int msPerFrame = (int)Math.round(1000.0 / this.config.getInt("maxFps"));
-        long timeNow = System.currentTimeMillis();
+        long timeDelta = 0;
 
         while (!this.shouldQuit) {
+            long frameStart = System.currentTimeMillis();
+
             StdDraw.clear(StdDraw.BLACK);
-            timeNow = System.currentTimeMillis();
+
+            for (Critter critter : this.critters) {
+                critter.advance(timeDelta);
+                critter.draw();
+            }
+
+            String frameTime = Long.toString(timeDelta) + "ms";
+            String fps = Long.toString((long)(1000.0 / timeDelta)) + "fps";
+            StdDraw.setPenColor(StdDraw.WHITE);
+            StdDraw.text(0.8, 0.9, frameTime);
+            StdDraw.text(0.8, 0.85, fps);
+
             StdDraw.show();
-            long timeDelta = System.currentTimeMillis() - timeNow;
-            StdDraw.pause((int)Math.max(msPerFrame - timeDelta, 0));
+
+            long frameDelta = System.currentTimeMillis() - frameStart;
+            StdDraw.pause((int)Math.max(msPerFrame - frameDelta, 0));
+
+            timeDelta = System.currentTimeMillis() - frameStart;
         }
     }
 
@@ -25,9 +49,8 @@ public class InvaderGameState {
         StdDraw.enableDoubleBuffering();
         StdDraw.setCanvasSize(config.getInt("windowWidth"),
                               config.getInt("windowHeight"));
+        StdDraw.setScale(-1, 1);
     }
-
-    public InvaderGameState(Config config) { this.config = config; }
 
     public void start() {
         this.initGameState();
--- src/cosmic_conquistadors/Missile.java
+++ src/cosmic_conquistadors/Missile.java
@@ -1,5 +1,21 @@
 package cosmic_conquistadors;
 
+import edu.princeton.cs.introcs.StdDraw;
+
 public class Missile extends DefaultCritter {
-    public Missile() {}
+    private double MISSILE_SPEED = 0.001;
+
+    public Missile(Vector position, double angle) {
+        this.setPosition(position);
+        double horizontalVelocity = MISSILE_SPEED * Math.cos(angle);
+        double verticalVelocity = MISSILE_SPEED * Math.sin(angle);
+        this.setVelocity(new Vector(horizontalVelocity, verticalVelocity));
+    }
+
+    @Override
+    public void draw() {
+        StdDraw.setPenColor(StdDraw.RED);
+        Vector position = this.getPosition();
+        StdDraw.filledRectangle(position.getX(), position.getY(), 0.025, 0.025);
+    }
 }
